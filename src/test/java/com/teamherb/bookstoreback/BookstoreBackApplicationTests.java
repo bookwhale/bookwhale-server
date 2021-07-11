7053d0907ed04f06ddc4d95d4eb34aec39c0fe02
highright96 <nsw5296@naver.com>
2021-07-11 23:11:01 +0900

    init : 프로젝트 세팅

---
+++ src/test/java/com/teamherb/bookstoreback/BookstoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamherb.bookstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
