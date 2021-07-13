b697f5eb138f67aa590d5944992e7d971230fcb5
highright96 <nsw5296@naver.com>
2021-07-13 19:17:01 +0900

    feature : 장바구니 엔티티 추가

---
+++ src/main/java/com/teamherb/bookstoreback/basket/domain/Basket.java
@@ -0,0 +1,35 @@
+package com.teamherb.bookstoreback.basket.domain;
+
+import com.teamherb.bookstoreback.post.domain.Post;
+import com.teamherb.bookstoreback.common.BaseEntity;
+import com.teamherb.bookstoreback.user.domain.User;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Basket extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "basket_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchaser_id")
+    private User purchaser;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
