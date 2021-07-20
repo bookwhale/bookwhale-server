2524087e468fe27ca4b5b3fb3f9803bf520beb6f
highright96 <nsw5296@naver.com>
2021-07-21 00:52:49 +0900

    feat : 로그인 요청, 응답 dto 추가

---
+++ src/main/java/com/teamherb/bookstoreback/user/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.teamherb.bookstoreback.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    private String identity;
+    private String password;
+}
