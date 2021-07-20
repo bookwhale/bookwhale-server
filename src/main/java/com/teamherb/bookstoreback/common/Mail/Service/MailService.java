0c592f1b2981803251f7387b16ffc87f9dc84dc6
Hose Joo <37391598+hose0728@users.noreply.github.com>
2021-07-20 22:01:06 +0900

    mail 기능 추가
    mail 보내는 기능 추가하였습니다.

--- src/main/java/com/teamherb/bookstoreback/common/Mail/Service/MailService.java
+++ src/main/java/com/teamherb/bookstoreback/common/Mail/Service/MailService.java
@@ -1,5 +1,6 @@
 package com.teamherb.bookstoreback.common.Mail.Service;
 
+import com.teamherb.bookstoreback.common.Mail.MailHandler;
 import com.teamherb.bookstoreback.common.Mail.domain.Mail;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,16 +14,35 @@ public class MailService {
     private static final String FROM_ADDRESS = "hose123@ajou.ac.kr";
 
     public void mailSend(Mail mail) {
-        System.out.println("메일 출력");
-        System.out.println("mail.getAddress() = " + mail.getAddress());
-        System.out.println("mail.getMessage() = " + mail.getMessage());
-        System.out.println("mail.getTitle() = " + mail.getTitle());
-        SimpleMailMessage message = new SimpleMailMessage();
+
+        /*SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(mail.getAddress());
         message.setFrom(MailService.FROM_ADDRESS);
         message.setSubject(mail.getTitle());
         message.setText(mail.getMessage());
 
-        mailSender.send(message);
+        mailSender.send(message);*/
+        try {
+            MailHandler mailHandler = new MailHandler(mailSender);
+
+            // 받는 사람
+            mailHandler.setTo(mail.getAddress());
+            // 보내는 사람
+            mailHandler.setFrom(MailService.FROM_ADDRESS);
+            // 제목
+            mailHandler.setSubject(mail.getTitle());
+            // HTML Layout
+            String htmlContent = "<p>" + mail.getMessage() +"<p> <img src='cid:teamherb'>";
+            mailHandler.setText(htmlContent, true);
+            // 첨부 파일
+            //mailHandler.setAttach("newTest.txt", "static/originTest.txt");
+            // 이미지 삽입
+            mailHandler.setInline("teamherb", "static/teamherb.png");
+
+            mailHandler.send();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
