e9a61121fa243c2410e9282fe745e317e646cc2d
highright96 <nsw5296@naver.com>
2021-07-24 13:34:52 +0900

    feat : Spring REST Docs 추가

--- src/main/java/com/teamherb/bookstoreback/user/dto/LoginRequest.java
+++ src/main/java/com/teamherb/bookstoreback/user/dto/LoginRequest.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
 
+    @NotBlank
     private String identity;
+
+    @NotBlank
     private String password;
 }
--- src/test/java/com/teamherb/bookstoreback/user/controller/UserControllerTest.java
+++ src/test/java/com/teamherb/bookstoreback/user/controller/UserControllerTest.java
@@ -1,11 +1,5 @@
 package com.teamherb.bookstoreback.user.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.teamherb.bookstoreback.account.dto.AccountRequest;
 import com.teamherb.bookstoreback.common.controller.CommonApiTest;
 import com.teamherb.bookstoreback.user.dto.SignUpRequest;
@@ -15,6 +9,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("유저 단위 테스트(Controller)")
 @WebMvcTest(controllers = UserController.class)
@@ -27,25 +30,36 @@ public class UserControllerTest extends CommonApiTest {
     @Test
     void createUser() throws Exception {
         AccountRequest accountRequest = AccountRequest.builder()
-            .accountBank("국민은행")
-            .accountNumber("123-1234-12345")
-            .accountOwner("남상우")
-            .build();
+                .accountBank("국민은행")
+                .accountNumber("123-1234-12345")
+                .accountOwner("남상우")
+                .build();
 
         SignUpRequest signUpRequest = SignUpRequest.builder()
-            .identity("highright96")
-            .password("1234")
-            .name("남상우")
-            .email("highright96@email.com")
-            .accountRequest(accountRequest)
-            .build();
+                .identity("highright96")
+                .password("1234")
+                .name("남상우")
+                .email("highright96@email.com")
+                .accountRequest(accountRequest)
+                .build();
 
         when(userService.createUser(any())).thenReturn(1L);
 
         mockMvc.perform(post("/api/user/signup")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(signUpRequest)))
-            .andExpect(status().isCreated())
-            .andDo(print());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andDo(document("user/signup",
+                        requestFields(
+                                fieldWithPath("identity").description("아이디"),
+                                fieldWithPath("password").description("비밀번호"),
+                                fieldWithPath("name").description("이름"),
+                                fieldWithPath("email").description("이메일"),
+                                fieldWithPath("accountRequest.accountNumber").description("계좌번호"),
+                                fieldWithPath("accountRequest.accountBank").description("은행"),
+                                fieldWithPath("accountRequest.accountOwner").description("이름")
+                        )
+                ));
     }
 }
