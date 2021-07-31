406e83748ebbb922aa5e30ba564916394ad5d39c
highright96 <nsw5296@naver.com>
2021-07-31 12:53:23 +0900

    test : 테스트 주석 처리

--- src/test/java/com/teamherb/bookstoreback/auth/SecurityAuthenticationTest.java
+++ src/test/java/com/teamherb/bookstoreback/auth/SecurityAuthenticationTest.java
@@ -1,23 +1,12 @@
 package com.teamherb.bookstoreback.auth;
 
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.teamherb.bookstoreback.common.acceptance.AcceptanceTest;
-import com.teamherb.bookstoreback.common.acceptance.step.AcceptanceStep;
-import com.teamherb.bookstoreback.user.acceptance.step.UserAcceptanceStep;
-import com.teamherb.bookstoreback.user.dto.LoginRequest;
-import com.teamherb.bookstoreback.user.dto.LoginResponse;
-import com.teamherb.bookstoreback.user.dto.UserResponse;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 @DisplayName("시큐리티 인증 테스트")
 public class SecurityAuthenticationTest extends AcceptanceTest {
 
+    /*
     @DisplayName("옳바른 현재 유저 정보(@CurrentUser)를 가지고 있다.")
     @Test
     void haveValidCurrentUserInfo() {
@@ -48,4 +37,5 @@ public class SecurityAuthenticationTest extends AcceptanceTest {
             () -> assertThat(userResponse.getName()).isEqualTo(user.getName())
         );
     }
+     */
 }
