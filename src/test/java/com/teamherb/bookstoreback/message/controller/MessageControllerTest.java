6c515ff79f9e4019ce95b9be8dc3be84c20d07bb
highright96 <nsw5296@naver.com>
2021-09-24 16:53:25 +0900

    docs : 메세지 API 문서화

--- src/test/java/com/teamherb/bookstoreback/message/controller/MessageControllerTest.java
+++ src/test/java/com/teamherb/bookstoreback/message/controller/MessageControllerTest.java
@@ -1,16 +1,15 @@
 package com.teamherb.bookstoreback.message.controller;
 
-import static java.lang.String.format;
 import static java.util.List.of;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.teamherb.bookstoreback.common.Pagination;
 import com.teamherb.bookstoreback.common.controller.CommonApiTest;
 import com.teamherb.bookstoreback.common.security.WithMockCustomUser;
+import com.teamherb.bookstoreback.message.docs.MessageDocumentation;
 import com.teamherb.bookstoreback.message.dto.MessageResponse;
 import com.teamherb.bookstoreback.message.service.MessageService;
 import java.time.LocalDateTime;
@@ -19,6 +18,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 
 @DisplayName("메세지 단위 테스트(Controller)")
 @WebMvcTest(controllers = MessageController.class)
@@ -26,6 +27,9 @@ public class MessageControllerTest extends CommonApiTest {
 
   @MockBean
   MessageService messageService;
+
+  @MockBean
+  SimpMessagingTemplate messagingTemplate;
 
   @Test
   @DisplayName("채팅방의 이전 메세지들을 조회한다.")
@@ -42,12 +46,13 @@ public class MessageControllerTest extends CommonApiTest {
 
     when(messageService.findMessages(any(), any())).thenReturn(of(messageResponse));
 
-    mockMvc.perform(get(format("/api/chat/room/%d/messages?page=%d&size=%d", roomId,
-            pagination.getPage(), pagination.getSize()))
+    mockMvc.perform(RestDocumentationRequestBuilders.get(
+                "/api/chat/room/{roomId}/messages?page={page}&size={size}", roomId,
+                pagination.getPage(), pagination.getSize())
             .header(HttpHeaders.AUTHORIZATION, "accessToken"))
         .andExpect(status().isOk())
-        .andDo(print());
-    //.andDo(PostDocumentation.findNaverBooks());
+        .andDo(print())
+        .andDo(MessageDocumentation.findMessages());
   }
 
   @Test
@@ -64,10 +69,11 @@ public class MessageControllerTest extends CommonApiTest {
 
     when(messageService.findLastMessage(any())).thenReturn(messageResponse);
 
-    mockMvc.perform(get(format("/api/chat/room/%d/last-message", roomId))
-            .header(HttpHeaders.AUTHORIZATION, "accessToken"))
+    mockMvc.perform(
+            RestDocumentationRequestBuilders.get("/api/chat/room/{roomId}/last-message", roomId)
+                .header(HttpHeaders.AUTHORIZATION, "accessToken"))
         .andExpect(status().isOk())
-        .andDo(print());
-    //.andDo(PostDocumentation.findNaverBooks());
+        .andDo(print())
+        .andDo(MessageDocumentation.findLastMessage());
   }
 }
