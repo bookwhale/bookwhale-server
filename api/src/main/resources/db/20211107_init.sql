3f8dd0ebda06568bf9b3ed0e0ff90555514163fe
gentledot <gentledot.wp@gmail.com>
2021-11-07 20:23:04 +0900

    refactor : 변경한 schema 내 comment 추가

--- api/src/main/resources/db/20211107_init.sql
+++ api/src/main/resources/db/20211107_init.sql
@@ -45,13 +45,13 @@ create table if not exists image
 
 create table if not exists post_like
 (
-    like_id        bigint not null auto_increment,
-    created_date       timestamp,
-    last_modified_date timestamp,
-    post_id            bigint not null,
-    user_id            bigint not null,
+    like_id        bigint not null auto_increment comment '관심(좋아요) ID',
+    created_date       timestamp comment '생성일시',
+    last_modified_date timestamp comment '마지막 변경일시',
+    post_id            bigint not null comment '판매글 id',
+    user_id            bigint not null comment '사용자 id',
     primary key (like_id)
-) engine = InnoDB;
+) comment '관심목록' engine = InnoDB;
 
 create table if not exists chat_room
 (
