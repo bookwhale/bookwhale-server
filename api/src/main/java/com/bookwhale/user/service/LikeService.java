452f1798d20a7a37cd2a59f61ac955da535782ee
gentledot <gentledot.wp@gmail.com>
2021-11-07 20:41:39 +0900

    refactor : LikeService 내 주석에서 좋아요 -> 관심으로 변경

--- api/src/main/java/com/bookwhale/user/service/LikeService.java
+++ api/src/main/java/com/bookwhale/user/service/LikeService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 접속 중인 사용자(user)가 게시글(post)에 좋아요를 남긴 요청을 처리
+ * 접속 중인 사용자(user)의 관심목록 관련 요청을 처리
  */
 @RequiredArgsConstructor
 @Transactional
@@ -25,10 +25,10 @@ public class LikeService {
   private final UserService userService;
 
   /**
-   * 현재 접속중인 사용자가 대상 post에 좋아요 추가
+   * 현재 접속중인 사용자가 대상 판매글(post)에 관심 추가
    *
    * @param user    현재 접속 중인 사용자
-   * @param request 좋아요 처리 요청
+   * @param request 관심목록 추가 처리 요청
    */
   public void addLike(User user, LikeRequest request) {
     Post post = userService.validatePostIdAndGetPost(request.getPostId());
@@ -43,7 +43,7 @@ public class LikeService {
   }
 
   /**
-   * 사용자가 선택한 좋아요를 취소 처리
+   * 사용자의 관심목록 중 선택한 관심을 취소 처리
    *
    * @param user   현재 접속 중인 사용자
    * @param likeId 좋아요 id
@@ -60,7 +60,7 @@ public class LikeService {
   }
 
   /**
-   * 사용자의 좋아요 목록을 전체 조회
+   * 사용자의 관심목록을 전체 조회
    *
    * @param user 현재 접속 중인 사용자
    * @return 좋아요 id에 해당하는 Post 정보로 구성된 LikeResponse 를 반환 (likeId, PostsResponse)
