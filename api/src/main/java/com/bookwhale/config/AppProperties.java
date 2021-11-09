f5b903221b39ca69ddd2d1ca6275fc5e202c894b
highright96 <nsw5296@naver.com>
2021-11-09 20:18:17 +0900

    refactor : AppProperties 파일 생성

---
+++ api/src/main/java/com/bookwhale/config/AppProperties.java
@@ -0,0 +1,43 @@
+package com.bookwhale.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+  private final Auth auth = new Auth();
+  private final OAuth2 oauth2 = new OAuth2();
+  private final NaverBook naverBook = new NaverBook();
+
+  @Getter
+  @Setter
+  public static class Auth {
+
+    private String tokenSecret;
+    private long tokenExpirationMsec;
+  }
+
+  @Getter
+  public static final class OAuth2 {
+
+    private List<String> authorizedRedirectUris = new ArrayList<>();
+
+    public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
+      this.authorizedRedirectUris = authorizedRedirectUris;
+      return this;
+    }
+  }
+
+  @Getter
+  @Setter
+  public static final class NaverBook {
+
+    private String clientId;
+    private String clientSecret;
+  }
+}
