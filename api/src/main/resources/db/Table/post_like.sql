1e4b0c7ab77ccfc117354fde55604cba61820505
gentledot <gentledot.wp@gmail.com>
2021-11-11 22:17:31 +0900

    refactor : #57 - 수정한 Entity의 Table DDL을 별도로 분리하여 저장 (이력관리에 용이하고자 함)

---
+++ api/src/main/resources/db/Table/post_like.sql
@@ -0,0 +1,10 @@
+drop table if exists post_like;
+create table if not exists post_like
+(
+    like_id        bigint not null auto_increment comment '관심(좋아요) ID',
+    created_date       timestamp comment '생성일시',
+    last_modified_date timestamp comment '마지막 변경일시',
+    post_id            bigint not null comment '판매글 id',
+    user_id            bigint not null comment '사용자 id',
+    primary key (like_id)
+) comment '관심목록' engine = InnoDB;
