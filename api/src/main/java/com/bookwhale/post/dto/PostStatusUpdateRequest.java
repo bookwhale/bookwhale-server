74fa578eedb87fc90b1f80cf9ba3b453bb8cffec
gentledot <gentledot.wp@gmail.com>
2021-11-18 00:36:01 +0900

    refactor : #62, #67 - 판매글 등록 및 목록 조회 요청, 응답 객체 수정 (1)

--- api/src/main/java/com/bookwhale/post/dto/PostStatusUpdateRequest.java
+++ api/src/main/java/com/bookwhale/post/dto/PostStatusUpdateRequest.java
@@ -2,18 +2,24 @@ package com.bookwhale.post.dto;
 
 import com.bookwhale.common.validator.ValueOfEnum;
 import com.bookwhale.post.domain.PostStatus;
-
 import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class PostStatusUpdateRequest {
 
   @NotBlank
   @ValueOfEnum(enumClass = PostStatus.class)
   private String postStatus;
+
+  public String getPostStatus() {
+    return postStatus;
+  }
+
+  public PostStatus getPostStatusByName(String name) {
+    return PostStatus.valueOf(name);
+  }
 }
