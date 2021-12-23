0b23c4acd3f99944df9dc61e0266481bd2dd3f17
gentledot <gentledot.wp@gmail.com>
2021-12-24 08:15:52 +0900

    feature: naver 에서 전달받은 정보로 사용자 정보 구성

--- api/src/main/java/com/bookwhale/auth/domain/OAuthObjectConverter.java
+++ api/src/main/java/com/bookwhale/auth/domain/OAuthObjectConverter.java
@@ -2,19 +2,24 @@ package com.bookwhale.auth.domain;
 
 import com.bookwhale.auth.domain.info.UserInfo;
 import com.bookwhale.auth.domain.info.UserInfoFromGoogle;
+import com.bookwhale.auth.domain.info.UserInfoFromNaver;
 import com.bookwhale.auth.domain.provider.OAuthProviderType;
 import com.bookwhale.auth.domain.token.GoogleOAuthToken;
+import com.bookwhale.auth.domain.token.NaverOAuthToken;
 import com.bookwhale.auth.domain.token.OAuthToken;
 import com.bookwhale.common.exception.CustomException;
 import com.bookwhale.common.exception.ErrorCode;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 
 @Slf4j
 public class OAuthObjectConverter {
+
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static OAuthToken getTokenFromResponse(ResponseEntity<String> response,
@@ -25,12 +30,11 @@ public class OAuthObjectConverter {
             if (StringUtils.isEmpty(responseBody)) {
                 throw new CustomException(ErrorCode.INFORMATION_NOT_FOUND);
             }
-
+            String body = responseBody.replaceAll("\\R", "");
             if (providerType.equals(OAuthProviderType.GOOGLE)) {
-                String body = responseBody.replaceAll("\\R", "");
                 oAuthToken = objectMapper.readValue(body, GoogleOAuthToken.class);
             } else if (providerType.equals(OAuthProviderType.NAVER)) {
-                // TODO naver token 변환 추가
+                oAuthToken = objectMapper.readValue(body, NaverOAuthToken.class);
             }
         } catch (JsonProcessingException e) {
             log.debug("token converting failed.", e);
@@ -49,16 +53,23 @@ public class OAuthObjectConverter {
             if (StringUtils.isEmpty(responseBody)) {
                 throw new CustomException(ErrorCode.INFORMATION_NOT_FOUND);
             }
+            String body = responseBody.replaceAll("\\R", "");
 
             if (providerType.equals(OAuthProviderType.GOOGLE)) {
-                String body = responseBody.replaceAll("\\R", "");
                 userInfo = objectMapper.readValue(body, UserInfoFromGoogle.class);
             } else if (providerType.equals(OAuthProviderType.NAVER)) {
-                // TODO naver user정보 변환 추가
+                Map<String, Object> map = objectMapper.readValue(body, Map.class);
+                // key = "response"에서 유효한 정보 확인 가능
+                var infoMap = (LinkedHashMap<String, String>) map.get("response");
+
+                userInfo = objectMapper.convertValue(infoMap, UserInfoFromNaver.class);
             }
         } catch (JsonProcessingException e) {
             log.debug("token converting failed.", e);
             throw new CustomException(ErrorCode.INVALID_ACCESS_TOKEN);
+        } catch (Exception e) {
+            log.debug("unexpected exception occurred.", e);
+            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
 
         return userInfo;
---
+++ api/src/main/java/com/bookwhale/auth/domain/token/NaverOAuthToken.java
@@ -0,0 +1,30 @@
+package com.bookwhale.auth.domain.token;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class NaverOAuthToken implements OAuthToken {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private String expiresIn;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("accessToken", accessToken)
+            .append("refreshToken", refreshToken)
+            .append("tokenType", tokenType)
+            .append("expiresIn", expiresIn)
+            .toString();
+    }
+}
