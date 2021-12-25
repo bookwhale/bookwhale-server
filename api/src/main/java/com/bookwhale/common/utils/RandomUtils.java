6887153a933e9c5d62ca12cea029f364135bfd39
gentledot <gentledot.wp@gmail.com>
2021-12-26 06:51:21 +0900

    style : line formatting 확인(RandomUtils)

--- api/src/main/java/com/bookwhale/common/utils/RandomUtils.java
+++ api/src/main/java/com/bookwhale/common/utils/RandomUtils.java
@@ -4,8 +4,9 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 
 public class RandomUtils {
-    // https://stackoverflow.com/questions/41107/how-to-generate-a-random-alpha-numeric-string
+
     public static String createRandomString() {
+        // https://stackoverflow.com/questions/41107/how-to-generate-a-random-alpha-numeric-string
         SecureRandom random = new SecureRandom();
         byte[] array = new byte[16];
         random.nextBytes(array);
