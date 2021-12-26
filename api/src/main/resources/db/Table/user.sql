6a89ead3f2407ad7c16d43249c20b054292d1407
gentledot <gentledot.wp@gmail.com>
2021-12-26 21:48:28 +0900

    refactor : user entity 테이블 DDL 변경

---
+++ api/src/main/resources/db/Table/user.sql
@@ -0,0 +1,11 @@
+create table if not exists user
+(
+    user_id            bigint       not null auto_increment comment '사용자 키값',
+    email              varchar(255) not null comment '사용자 이메일',
+    nickname           varchar(255) not null comment '사용자 이름(닉네임)',
+    profile_image      varchar(255) comment '사용자 프로필 URL',
+    created_date       timestamp comment '생성일시',
+    last_modified_date timestamp comment '수정일시',
+    primary key (user_id),
+    unique index idx_user_email (email)
+) engine = InnoDB;
