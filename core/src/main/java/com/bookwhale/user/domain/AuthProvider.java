5b871c6b72bee8670d4c8a2d88375a7fd2efcf67
highright96 <nsw5296@naver.com>
2021-12-30 00:41:05 +0900

    refactor: 필요없는 클래스 제거

--- core/src/main/java/com/bookwhale/user/domain/AuthProvider.java
+++ core/src/main/java/com/bookwhale/user/domain/AuthProvider.java
@@ -1,7 +1,5 @@
 package com.bookwhale.user.domain;
 
 public enum AuthProvider {
-    LOCAL,
-    GOOGLE,
-    NAVER
+    GOOGLE, NAVER
 }
--- core/src/main/java/com/bookwhale/user/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package com.bookwhale.user.domain;
-
-public enum Role {
-    ROLE_USER, ROLE_ADMIN
-}
