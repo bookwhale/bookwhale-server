276cd1fb6bead8334441d76c7efafa65af456183
highright96 <nsw5296@naver.com>
2021-12-30 01:53:58 +0900

    refactor: sql 파일 정리 (#81)

--- api/src/main/resources/db/20211205_setting.sql
+++
@@ -1,9 +0,0 @@
-#schema 선택
-use bookwhale;
-
-#character utf8mb4 확인
-show variables like 'c%';
-
-#Timezone 확인 후 설정
-select @@time_zone, now();
-set time_zone = 'Asia/Seoul';
--- api/src/main/resources/db/table/token.sql
+++ api/src/main/resources/db/table/token.sql
@@ -6,4 +6,4 @@ create table if not exists token
     created_date       timestamp,
     last_modified_date timestamp,
     primary key (email)
-)
+)engine = InnoDB;
