7e5a847f2a9ed666415ce33c7b2e6311603ffb72
gentledot <gentledot.wp@gmail.com>
2022-04-10 11:41:29 +0900

    #167 - refactor : message push 전송 시 상대방에게 data가 갈 수 있도록 변경

--- core/src/main/java/com/bookwhale/push/service/PushProcessor.java
+++ core/src/main/java/com/bookwhale/push/service/PushProcessor.java
@@ -3,12 +3,17 @@ package com.bookwhale.push.service;
 import com.bookwhale.article.domain.Article;
 import com.bookwhale.chatroom.domain.ChatRoom;
 import com.bookwhale.chatroom.domain.ChatRoomRepository;
+import com.bookwhale.chatroom.domain.Opponent;
+import com.bookwhale.common.exception.CustomException;
+import com.bookwhale.common.exception.ErrorCode;
 import com.bookwhale.message.domain.Message;
 import com.bookwhale.push.dto.PushMessageParams;
 import com.bookwhale.push.dto.PushMessageParams.PushMessageParamsBuilder;
 import com.bookwhale.user.domain.User;
+import com.bookwhale.user.domain.UserRepository;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -48,10 +53,6 @@ public class PushProcessor {
 
     public void pushMessageOfChatMessage(Message chatMessage) {
         String message = chatMessage.getContent();
-        PushMessageParamsBuilder createChatRoomPushMessage = PushMessageParams.builder()
-            .title(String.format("%s 님의 메시지", chatMessage.getSenderIdentity()))
-            .body(message);
-
         Long roomId = chatMessage.getRoomId();
         Long senderId = chatMessage.getSenderId();
 
@@ -59,20 +60,24 @@ public class PushProcessor {
             ChatRoom chatRoom = chatRoomRepository.findById(roomId)
                 .orElseThrow(() -> new RuntimeException("room 확인 실패"));
 
+            User buyer = chatRoom.getBuyer();
+            User seller = chatRoom.getSeller();
+
             Map<String, String> dataMap = new HashMap<>();
             dataMap.put("message ", message);
             dataMap.put("roomId", roomId.toString());
             dataMap.put("senderId", senderId.toString());
             dataMap.put("articleTitle", chatRoom.getArticle().getTitle());
 
-            pushService.sendMessageFromFCM(
-                createChatRoomPushMessage.targetToken(chatRoom.getBuyer().getDeviceToken())
-                    .build(), dataMap
-            );
-            pushService.sendMessageFromFCM(
-                createChatRoomPushMessage.targetToken(chatRoom.getSeller().getDeviceToken())
-                    .build(), dataMap
-            );
+            if (senderId.equals(buyer.getId())) {
+                pushService.sendMessageFromFCMWithoutNonitication(
+                    seller.getDeviceToken(), dataMap
+                );
+            } else {
+                pushService.sendMessageFromFCMWithoutNonitication(
+                    buyer.getDeviceToken(), dataMap
+                );
+            }
         } catch (Exception e) {
             log.error("채팅 메시지 알림 동작에 오류가 발생하였습니다.", e);
         }
